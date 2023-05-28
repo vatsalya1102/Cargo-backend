ab3644d558df1fc9d3a1019f381af89c4b07edc3
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-05-28 11:44:25 +0530

    Backend: Initialized project structure.
    Added required dependencies like body-parser,cors,mongoose.
    Added controllers, middleware, models, routes.

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import userRoutes from './routes/users.js'
+
+const app = express();
+
+app.use(bodyParser.json({ extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.use('/user', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(`${process.env.CONNECTION_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    })
+    .catch((error) => {
+        console.log(error);
+    });
