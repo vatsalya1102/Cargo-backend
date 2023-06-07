61bff685c799f530c72eaa89268405c00ddbfffe
Vatsalya Sharma <vatsalya2021.official@gmail.com>
2023-06-07 22:39:44 +0530

    Backend: Minor update

--- controller/users.js
+++ controller/users.js
@@ -9,7 +9,9 @@ export const signin = async (req, res) => {
     try {
         const existingUser = await User.findOne({ email });
 
-        if (!existingUser) return res.status(404).json({ message: "User doesn't exist !" });
+        if (!existingUser) {
+            return res.status(404).json({ message: "User doesn't exist !" });
+        }
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
         if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
         const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" });
@@ -36,7 +38,7 @@ export const signup = async (req, res) => {
 
 export const getTransporters = async (req, res) => {
     try {
-        const transporters = await User.find({role: 'Transporter'});
+        const transporters = await User.find({ role: 'Transporter' });
         res.status(200).json(transporters);
     } catch (error) {
         res.status(404).json({ message: error.message })
